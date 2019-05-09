f6482357ba8a140a3aae736b7818ef967fa6dd5d
mag16 <marcoguzman16@gmail.com>
2019-05-08 19:26:09 -0700

    HeaderTitle

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import HeaderTitle from './components/HeaderComponents/HeaderTitle'
 
 const App = () => {
     return (
         <div>
+            <HeaderTitle/>
             <h3>Welcome to React Social Card!</h3>
             <p>
                 Begin by exploring the `components` directory. You'll notice we have a
